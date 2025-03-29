71c9fed5cbeef44f9ad6e09fa44d62b2241ce565
Karthik Maddineni <119654411+karthikmaddineni@users.noreply.github.com>
2025-03-28 21:44:17 -0400

    Update Exp.jsx

--- src/components/Exp.jsx
+++ src/components/Exp.jsx
@@ -8,6 +8,32 @@ return (
     <div class="experience-grid">
         <div class="experience-card">
             <div class="experience-image">
+                <img src={exp1} alt="MOURI" />
+            </div>
+            <div class="experience-content">
+                <h3 class="experience-title">Java Developer</h3>
+                <p class="">I was tasked with developing and optimizing backend services using Spring Boot and Hibernate. One of my first major projects was building a RESTful API for a financial application, where I had to ensure high performance and security.</p>
+                <div class="experience-tags">
+                    <span class="experience-tag">Java</span>
+                    <span class="experience-tag">React</span>
+                    <span class="experience-tag">Micro services </span>
+                    <span class="experience-tag">CI/CD</span>
+                    <span class="experience-tag">SDLC</span>
+                </div>
+            </div>
+            
+            <div class="experience-overlay">
+                <h3 class="overlay-title">Java Developer</h3>
+                <p class="overlay-description">Got involved in microservices architecture, breaking down a monolithic application into independent services, which improved scalability, contributed to CI/CD pipelines using Jenkins and containerized applications with Docker</p>
+                <ul class="overlay-features">
+                    <li>Developed the IoT-Based Smart Warehouse Management System, integrating IoT sensors with AWS IoT Core to enable real-time inventory tracking, reducing stock discrepancies by 30%.</li>
+                    <li>Engineered a React.js dashboard with interactive charts and WebSocket-based updates, allowing warehouse managers to monitor stock movement seamlessly.</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="experience-card">
+            <div class="experience-image">
                 <img src={exp1} alt="EDUSKILLS" />
             </div>
             <div class="experience-content">
